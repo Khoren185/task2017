5896e48b38435886b1f8756db926daefa7aa1099
Khoren185 <xoren185@gmail.com>
2017-01-12 18:44:52 +0400

    wedding machine solution

--- wedding-machine-step-1/script.js
+++ wedding-machine-step-1/script.js
@@ -1,14 +1,14 @@
-var favDrink = prompt("What's your favorite drink?");
+var favDrink = prompt("What's your favorite drink\r\n1.Coca-Cola\r\n2.Sprite\r\n3.Mirinda");
 switch(favDrink) {
         case "1":
-        text = "duq yntrel eq Coca-Cola,vori arjeqn e 300 dram";
+        text = "You chouse is Coca-Cola and his value are 300AMD";
         break;
         case "2":
-        text= "duq yntrel eq Sprite,vori arjeqn e 250 dram";
+        text= "You chouse is Sprite and his value are 300AMD";
         break;
         case "3":
-        text ="duq yntrel eq Sprite,vori arjeqn e 250 dram";
+        text ="You chouse is Mirinda and his value are 300AMD";
         default:
-        text = "Knereq bayc ays hamarov xmichq menq chunenq";
+        text = "Sorry we don't have this number of drinks";
 }
 alert(text);
